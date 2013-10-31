e2542c6301f2cd02aed30df194b299ada5b61312
xoyip <xoyip@piyox.info>
2013-10-31 20:15:06 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HatebuWeb::Application.config.secret_key_base = 'a654d782fea1553050ecd84f2347271f0a27dd635ad27963f71a829b990cb6d1d77ecd6e465710bf0643669fddf5dded85d28c9d1db654f2cdcddb86d0f87ab4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+HatebuWeb::Application.config.session_store :cookie_store, key: '_hatebu-web_session'
