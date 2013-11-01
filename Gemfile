b51a0900c5cbc1a395548bf6c1a5980e6fae8e83
xoyip <xoyip@piyox.info>
2013-11-01 11:37:54 +0900

    prepare for rspec

--- Gemfile
+++ Gemfile
@@ -1,58 +1,50 @@
 source 'https://rubygems.org'
 
-# Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.0.0'
-
-# Use sqlite3 as the database for Active Record
-# gem 'sqlite3'
 gem 'mysql2'
-
-# privacy
 gem 'figaro'
-
-# Use devise and omniauth
 gem 'devise'
 gem 'omniauth-hatena'
 
-# Use Haml for templates
-gem 'erb2haml'
-gem 'haml-rails'
+group :development do
+  gem 'growl'
+  gem 'guard-rspec'
+  gem 'guard-spork'
+  gem 'database_cleaner'
+  gem 'quiet_assets'
+  gem 'pry-debugger'
+end
 
-# Use SCSS for stylesheets
-gem 'sass-rails', '~> 4.0.0'
-gem 'bootstrap-sass'
+group :development, :test do
+  gem 'rspec'
+  gem 'rspec-rails'
+  gem 'spork-rails', github: 'railstutorial/spork-rails'
+  gem 'simplecov', :require => false
+  gem 'simplecov-rcov', :require => false
+  gem 'capybara'
+  gem 'capybara-webkit'
+  gem 'poltergeist'
+  gem 'launchy'
+  gem 'turnip'
+  gem 'selenium-webdriver'
+  gem 'debugger'
+end
 
-# Use Uglifier as compressor for JavaScript assets
-gem 'uglifier', '>= 1.3.0'
+group :assets do
+  gem 'erb2haml'
+  gem 'haml-rails'
+  gem 'coffee-rails', '~> 4.0.0'
+  gem 'sass-rails', '~> 4.0.0'
+  gem 'bootstrap-sass'
+  gem 'uglifier', '>= 1.3.0'
+end
 
-# Use CoffeeScript for .js.coffee assets and views
-gem 'coffee-rails', '~> 4.0.0'
-
-# See https://github.com/sstephenson/execjs#readme for more supported runtimes
-# gem 'therubyracer', platforms: :ruby
-
-# Use jquery as the JavaScript library
 gem 'jquery-rails'
-
-# Turbolinks makes following links in your web application faster. Read more: https://github.com/rails/turbolinks
-# gem 'turbolinks'
-
-# Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
 gem 'jbuilder', '~> 1.2'
+
 
 group :doc do
   # bundle exec rake doc:rails generates the API under doc/api.
   gem 'sdoc', require: false
 end
 
-# Use ActiveModel has_secure_password
-# gem 'bcrypt-ruby', '~> 3.0.0'
-
-# Use unicorn as the app server
-# gem 'unicorn'
-
-# Use Capistrano for deployment
-# gem 'capistrano', group: :development
-
-# Use debugger
-gem 'debugger', group: [:development, :test]
---
+++ spec/spec_helper.rb
@@ -0,0 +1,109 @@
+require 'rubygems'
+require 'spork'
+require 'debugger'
+
+Spork.prefork do
+  # This file is copied to spec/ when you run 'rails generate rspec:install'
+  ENV["RAILS_ENV"] ||= 'test'
+  require File.expand_path("../../config/environment", __FILE__)
+
+  require 'capybara/dsl'
+  require 'capybara/rspec'
+  require 'capybara/webkit'
+  require 'capybara/poltergeist'
+  require 'rspec/rails'
+  require 'rspec/autorun'
+  require 'turnip'
+  require 'turnip/capybara'
+  require 'database_cleaner'
+
+  Capybara.javascript_driver = :poltergeist
+
+  unless ENV['DRB']
+    require 'simplecov'
+    require 'simplecov-rcov'
+    SimpleCov.formatter = SimpleCov::Formatter::RcovFormatter
+    SimpleCov.start 'rails'
+  end
+
+
+  # Requires supporting ruby files with custom matchers and macros, etc,
+  # in spec/support/ and its subdirectories.
+  Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
+
+  # Checks for pending migrations before tests are run.
+  # If you are not using ActiveRecord, you can remove this line.
+  ActiveRecord::Migration.check_pending! if defined?(ActiveRecord::Migration)
+
+  RSpec.configure do |config|
+    # ## Mock Framework
+    #
+    # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
+    #
+    # config.mock_with :mocha
+    # config.mock_with :flexmock
+    # config.mock_with :rr
+
+    # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
+    config.fixture_path = "#{::Rails.root}/spec/fixtures"
+
+    # If you're not using ActiveRecord, or you'd prefer not to run each of your
+    # examples within a transaction, remove the following line or assign false
+    # instead of true.
+    config.use_transactional_fixtures = true
+
+    # If true, the base class of anonymous controllers will be inferred
+    # automatically. This will be the default behavior in future versions of
+    # rspec-rails.
+    config.infer_base_class_for_anonymous_controllers = false
+
+    # Run specs in random order to surface order dependencies. If you find an
+    # order dependency and want to debug it, you can fix the order by providing
+    # the seed, which is printed after each run.
+    #     --seed 1234
+    config.order = "random"
+    
+    config.include Capybara::DSL, :type => :request
+    config.include Capybara::RSpecMatchers, :type => :request    
+
+    # jsのテストのためにCapybaraで実際のブラウザを立ち上げているが、
+    # そのときにDBへ接続できなくて起きるエラーを回避するため以下の設定が必要
+    config.use_transactional_fixtures = false
+    config.before(:suite) do
+      DatabaseCleaner.clean_with :truncation
+    end
+
+    config.before(:each) do
+      if example.metadata[:js]
+        DatabaseCleaner.strategy = :truncation
+      else
+        DatabaseCleaner.strategy = :transaction
+      end
+      DatabaseCleaner.start
+    end
+
+    config.after(:each) do
+      DatabaseCleaner.clean
+    end
+    
+  end
+
+end
+
+Spork.each_run do
+  FactoryGirl.reload
+  Dir.glob("spec/**/*steps.rb") {|f| load f, true}
+
+  if Spork.using_spork?
+    Rails.application.reloaders.each{|reloader| reloader.execute_if_updated }
+  end
+
+  unless ENV['DRB']
+    require 'simplecov'
+    require 'simplecov-rcov'
+    SimpleCov.formatter = SimpleCov::Formatter::RcovFormatter
+    SimpleCov.start 'rails'
+  end
+  
+end
+  
