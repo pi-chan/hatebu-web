ed95a479913ed8cc626eafe7951b6191430b4a07
xoyip <xoyip@piyox.info>
2013-11-01 10:32:33 +0900

    login succeeded

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,13 @@
 class PagesController < ApplicationController
   def home
+    unless user_signed_in?
+      redirect_to :login
+    end
+  end
+
+  def login
+    if user_signed_in?
+      redirecto_to :home
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,11 @@ HatebuWeb::Application.routes.draw do
   devise_for :users, controllers: {omniauth_callbacks: "users/omniauth_callbacks"}
   devise_scope :user do
     get 'sign_in', to: 'devise/sessions#new', as: :new_user_session
-    get 'sign_out', to: 'devise/session#destroy', as: :destroy_user_session
+    get 'sign_out', to: 'devise/sessions#destroy', as: :destroy_user_session
   end
 
   root 'pages#home'
+  get 'login' => 'pages#login'
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
